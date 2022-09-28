a132fa21716fc3a2c9296e892bae474e0bf41cd2
Álvaro Cabana <user.name>
2022-09-28 16:18:29 -0300

    datos: primer chequeo de formato

--- scripts/levantaDatos.r
+++ scripts/levantaDatos.r
@@ -1,9 +1,12 @@
 library(tidyverse)
 
 apertura_2011 = read.csv("datos/Datos Apertura Presupuestal UdelaR - 2022-2011.csv")
-apertura_2011$Servicio %>% table()
+cambio_nombres_servicio = read.csv("datos/cambio de nombres de servicio.csv")
 
-apertura_2011 %>% 
-  select(starts_with("Servicio")) %>% 
-  distinct() %>% 
-  write.csv("datos/cambio_servicio.csv")
+cambiador_nombres = 
+  cambio_nombres_servicio  %>% 
+    pull(Servicio)
+
+names(cambiador_nombres) = cambio_nombres_servicio$Servicio..codificación.original.
+
+apertura_2011$Servicio= cambiador_nombres[apertura_2011$Servicio]
